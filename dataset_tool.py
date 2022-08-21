875c9311d149e1555498be80f3778c63fa3c645a
Alex Bedulin <alexander.bedulin@gmail.com>
2022-08-21 13:32:55 +0300

    fixed create_from_images: removed shape check because it seems to fire wrong/unimportant error. Added progress update.

--- dataset_tool.py
+++ dataset_tool.py
@@ -696,7 +696,6 @@ def create_from_images(tfrecord_dir, image_dir, shuffle):
 
     img = np.asarray(PIL.Image.open(image_filenames[0]))
     print(img.shape)
-    shape = img.shape
     resolution = img.shape[0]
     channels = img.shape[2] if img.ndim == 3 else 1
     if img.shape[1] != resolution:
@@ -709,15 +708,14 @@ def create_from_images(tfrecord_dir, image_dir, shuffle):
     with TFRecordExporter(tfrecord_dir, len(image_filenames)) as tfr:
         order = tfr.choose_shuffled_order() if shuffle else np.arange(len(image_filenames))
         for idx in range(order.size):
+            if idx % 1000 == 0:
+                print("added images", idx, flush=True)
             img = np.asarray(PIL.Image.open(image_filenames[order[idx]]))
             if channels == 1:
                 print("Greyscale, adding dimension:", image_filenames[order[idx]], img.shape)
                 img = img[np.newaxis, :, :] # HW => CHW
             else:
                 img = img.transpose([2, 0, 1]) # HWC => CHW
-            if img.shape != shape:
-                print("Wrong shape:", image_filenames[order[idx]], img.shape, "should be", shape)
-                continue
             tfr.add_image(img)
 
 #----------------------------------------------------------------------------
